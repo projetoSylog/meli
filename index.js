6e6068f6f9f522edd31b0ae3315530324f3bfc19
projetoSylog <57667088+projetoSylog@users.noreply.github.com>
2025-07-09 12:06:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Servidor Node rodando!');
 });
+app.get('/home', (req, res) => {
+  res.send('Bem-vindo à rota /home!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
